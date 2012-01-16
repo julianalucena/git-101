b23e541a413ffe0add8901b9364bc5887746b6d0
Juliana Lucena <julianalucenaa@gmail.com>
2012-01-16 15:09:24 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitPlay::Application.config.secret_token = 'b22cfec3ad982a549420dc9b40a446adbaa46a6b2da88338b6a8574b75de24132a05781e3552419db11f5b00db5fa2e645891928f2fce91e452cfb86b9fafd2c'
